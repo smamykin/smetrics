5a15c645ec9de588db849cfdbeaa401ada2f092a
s.mamykin <mamykin@banki.ru>
2023-01-21 14:38:55 +0300

    increment 7 | flags

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/caarlos0/env/v6"
 	"github.com/smamykin/smetrics/internal/agent"
 	"github.com/smamykin/smetrics/internal/utils"
 	"log"
+	"os"
 	"time"
 )
 
 type Config struct {
-	Address        string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
+	Address        string        `env:"ADDRESS"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL"`
 }
 
+const (
+	defaultAddress        = "localhost:8080"
+	defaultReportInterval = time.Second * 10
+	defaultPollInterval   = time.Second * 2
+)
+
 func main() {
+	address := flag.String("a", defaultAddress, "The address of the metric server")
+	reportInterval := flag.Duration("r", defaultReportInterval, "How often to send metrics to server")
+	pollInterval := flag.Duration("p", defaultPollInterval, "How often to refresh metrics")
+	flag.Parse()
+
 	var cfg Config
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if _, isPresent := os.LookupEnv("ADDRESS"); !isPresent {
+		cfg.Address = *address
+	}
+	if _, isPresent := os.LookupEnv("REPORT_INTERVAL"); !isPresent {
+		cfg.ReportInterval = *reportInterval
+	}
+	if _, isPresent := os.LookupEnv("POLL_INTERVAL"); !isPresent {
+		cfg.PollInterval = *pollInterval
 	}
 
 	fmt.Printf("Starting the agent. The configuration: %#v", cfg)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/caarlos0/env/v6"
 	"github.com/smamykin/smetrics/internal/server/server"
 	"log"
+	"os"
 	"time"
 )
 
 type Config struct {
-	Address       string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	Restore       bool          `env:"RESTORE" envDefault:"true"`
-	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
+	Address       string        `env:"ADDRESS"`
+	Restore       bool          `env:"RESTORE"`
+	StoreFile     string        `env:"STORE_FILE"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL"`
 }
 
+const (
+	defaultAddress       = "localhost:8080"
+	defaultRestore       = true
+	defaultStoreFile     = "/tmp/devops-metrics-db.json"
+	defaultStoreInterval = time.Second * 300
+)
+
 func main() {
+	address := flag.String("a", defaultAddress, "The address of the server")
+	restore := flag.Bool("r", defaultRestore, "To restore the dump from the file")
+	storeFile := flag.String("f", defaultStoreFile, "the absolute path to the dump file.")
+	storeInterval := flag.Duration("i", defaultStoreInterval, "How often to save the dump of the metrics")
+	flag.Parse()
+
 	var cfg Config
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if _, isPresent := os.LookupEnv("ADDRESS"); !isPresent {
+		cfg.Address = *address
+	}
+	if _, isPresent := os.LookupEnv("RESTORE"); !isPresent {
+		cfg.Restore = *restore
+	}
+	if _, isPresent := os.LookupEnv("STORE_FILE"); !isPresent {
+		cfg.StoreFile = *storeFile
+	}
+	if _, isPresent := os.LookupEnv("STORE_INTERVAL"); !isPresent {
+		cfg.StoreInterval = *storeInterval
 	}
 
 	fmt.Printf("Starting the server. The configuration: %#v", cfg)
