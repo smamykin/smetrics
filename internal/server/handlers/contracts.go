519e7b61d8db0cb98f996a0640e4b550d8068611
s.mamykin <mamykin@banki.ru>
2023-02-05 13:08:16 +0300

    increment11 | added validation checks

--- internal/server/handlers/contracts.go
+++ internal/server/handlers/contracts.go
@@ -28,11 +28,11 @@ type IParametersBag interface {
 }
 
 type Metrics struct {
-	ID    string   `json:"id"`                             // имя метрики
-	MType string   `json:"type" valid:"in(gauge|counter)"` // параметр, принимающий значение gauge или counter
-	Delta *int64   `json:"delta,omitempty"`                // значение метрики в случае передачи counter
-	Value *float64 `json:"value,omitempty"`                // значение метрики в случае передачи gauge
-	Hash  string   `json:"hash,omitempty"`                 // значение хеш-функции
+	ID    string   `json:"id" valid:"required"`                     // имя метрики
+	MType string   `json:"type" valid:"in(gauge|counter),required"` // параметр, принимающий значение gauge или counter
+	Delta *int64   `json:"delta,omitempty"`                         // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"`                         // значение метрики в случае передачи gauge
+	Hash  string   `json:"hash,omitempty"`                          // значение хеш-функции
 }
 
 type GaugeMetric struct {
